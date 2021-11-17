52ae708b65f6f219fa554d63bfe4a6a9fc280f48
brunowqfernandes <bruno.wqfernandes@gmail.com>
2021-11-16 23:16:46 -0300

    correção de erro de produtos

---
+++ components/Loading/index.js
@@ -0,0 +1,7 @@
+export function Loading() {
+    return (
+        <div class="flex justify-center items-center">
+            <div class="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900" ></div>
+        </div>
+    )
+}
